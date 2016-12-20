22aeeb90053f9141484e68dd5170eabecbf1b260
stephengrider <ste.grider@gmail.com>
2016-12-20 14:17:56 -0800

    removed config

--- webpack.config.js
+++ webpack.config.js
@@ -6,27 +6,5 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-1']
-        }
-      },
-      {
-        test: /\.css$/,
-        loaders: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      }
-    ]
-  },
-  devServer: {
-    historyApiFallback: true
   }
 };
